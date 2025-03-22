5046526a2ebd89854d838ed2146ca98f04fa1543
onxlmao <ricoardiansyahpersional@gmail.com>
2025-03-23 02:07:45 +0700

    Update rvc.py

--- src/rvc.py
+++ src/rvc.py
@@ -2,7 +2,7 @@ from multiprocessing import cpu_count
 from pathlib import Path
 
 import torch
-from fairseq import checkpoint_utils
+
 from scipy.io import wavfile
 
 from infer_pack.models import (
@@ -30,26 +30,7 @@ class Config:
         if torch.cuda.is_available():
             i_device = int(self.device.split(":")[-1])
             self.gpu_name = torch.cuda.get_device_name(i_device)
-            if (
-                    ("16" in self.gpu_name and "V100" not in self.gpu_name.upper())
-                    or "P40" in self.gpu_name.upper()
-                    or "1060" in self.gpu_name
-                    or "1070" in self.gpu_name
-                    or "1080" in self.gpu_name
-            ):
-                print("16 series/10 series P40 forced single precision")
-                self.is_half = False
-                for config_file in ["32k.json", "40k.json", "48k.json"]:
-                    with open(BASE_DIR / "src" / "configs" / config_file, "r") as f:
-                        strr = f.read().replace("true", "false")
-                    with open(BASE_DIR / "src" / "configs" / config_file, "w") as f:
-                        f.write(strr)
-                with open(BASE_DIR / "src" / "trainset_preprocess_pipeline_print.py", "r") as f:
-                    strr = f.read().replace("3.7", "3.0")
-                with open(BASE_DIR / "src" / "trainset_preprocess_pipeline_print.py", "w") as f:
-                    f.write(strr)
-            else:
-                self.gpu_name = None
+            
             self.gpu_mem = int(
                 torch.cuda.get_device_properties(i_device).total_memory
                 / 1024
@@ -57,11 +38,7 @@ class Config:
                 / 1024
                 + 0.4
             )
-            if self.gpu_mem <= 4:
-                with open(BASE_DIR / "src" / "trainset_preprocess_pipeline_print.py", "r") as f:
-                    strr = f.read().replace("3.7", "3.0")
-                with open(BASE_DIR / "src" / "trainset_preprocess_pipeline_print.py", "w") as f:
-                    f.write(strr)
+            
         elif torch.backends.mps.is_available():
             print("No supported N-card found, use MPS for inference")
             self.device = "mps"
@@ -96,6 +73,7 @@ class Config:
 
 
 def load_hubert(device, is_half, model_path):
+    from fairseq import checkpoint_utils
     models, saved_cfg, task = checkpoint_utils.load_model_ensemble_and_task([model_path], suffix='', )
     hubert = models[0]
     hubert = hubert.to(device)
